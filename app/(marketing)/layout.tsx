7de6e1c538bc94afbd7761b228ea7760a8d22e0d
ManishBisht7 <manish@mydahlia.app>
2023-07-22 12:40:22 +0530

    feat: project setup

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,25 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+interface MarketingLayoutProps {
+  children: React.ReactNode;
+}
+export default function MarketingLayout({ children }: MarketingLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="container z-40 bg-background mt-4 rounded-full">
+        <div className="flex h-20 items-center justify-between py-6">
+          <p className="font-heading md:text-2xl tracking-tight uppercase font-black text-slate-950">
+            Rhinos gym
+          </p>
+          navbar
+        </div>
+      </header>
+      <main className="flex-1">{children}</main>
+      footer
+      {/* <SiteFooter /> */}
+    </div>
+  );
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Props = {};
+
+export default function Page({}: Props) {
+  return (
+    <div>
+      <h1 className="font-heading text-8xl tracking-[.1rem] font-semibold uppercase text-slate-800">
+        improve your
+      </h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti,
+        error neque. Consectetur repudiandae sint excepturi assumenda distinctio
+        repellat vero facilis, corrupti minus repellendus in impedit architecto
+        eius nulla atque autem.
+      </p>
+    </div>
+  );
+}

54cdd6ab396d6045781fe05afe79533d83e3fa0e
ManishBisht7 <manishbisht9711@gmail.com>
2023-07-30 17:26:41 +0530

    fix: homepage text-color

--- app/(root)/(marketing)/page.tsx
+++ app/(root)/(marketing)/page.tsx
@@ -9,8 +9,8 @@ type Props = {};
 export default function Page({}: Props) {
   return (
     <main>
-      <section className="relative container p-10 bg-primary rounded-[3rem] mt-4">
-        <h1 className=" font-heading text-2xl tracking-tight font-semibold uppercase text-primary border-b-2 pb-2 border-black/70 offset">
+      <section className="relative container p-10 bg-primary text-primary-foreground rounded-[3rem] mt-4">
+        <h1 className=" font-heading text-2xl tracking-tight font-semibold  uppercase border-b-2 pb-2 border-black/70 offset">
           Make you body fit this spring with us
         </h1>
         <Image
@@ -22,7 +22,7 @@ export default function Page({}: Props) {
           height={150}
         />
 
-        <div className="flex flex-col gap-2 mt-6 text-9xl font-heading tracking-tighter uppercase text-primary ">
+        <div className="flex flex-col gap-2 mt-6 text-9xl font-heading tracking-tighter uppercase">
           <div className="flex gap-2 items-center">
             <p>Improve your</p>
             <Image
@@ -38,7 +38,7 @@ export default function Page({}: Props) {
           <div className="flex items-center justify-between">
             <div className="flex gap-6 items-center">
               <p>The better</p>
-              <p className="text-base text-primary/80 font-sans font-medium tracking-normal capitalize w-60">
+              <p className="text-base font-sans font-medium tracking-normal capitalize w-60">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis,
                 dicta?
               </p>
@@ -98,21 +98,21 @@ export default function Page({}: Props) {
         <Icons.Hexagon className="w-20 h-20 text-white/70" />
       </section>
 
-      <section className="container p-10">
-        <h3 className="font-heading text-6xl text-primary-foreground uppercase">
+      <section className="container p-10 text-primary">
+        <h3 className="font-heading text-6xl uppercase">
           Facilities at fitness rhinos
         </h3>
-        <p className="text-primary-foreground/80 text-sm mt-4 max-w-lg">
+        <p className="text-primary/80 text-sm mt-4 max-w-lg">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum non
           natus eveniet architecto fuga molestias similique? Voluptatem
           veritatis.
         </p>
 
-        <div className="grid grid-cols-3 gap-4 mt-6 text-primary-foreground">
+        <div className="grid grid-cols-3 gap-4 mt-6 ">
           <div className="bg-slate-900 relative rounded-3xl px-6 py-16 flex items-end flex-col gap-4">
             <Icons.CalendarDays className="w-12 h-12 text-slate-400" />
             <p className="text-4xl uppercase font-heading">timetable</p>
-            <p className="text-end text-primary-foreground/70">
+            <p className="text-end text-primary/70">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi,
               quisquam.
             </p>
@@ -123,7 +123,7 @@ export default function Page({}: Props) {
           <div className="bg-slate-900 relative rounded-3xl px-6 py-16 flex items-end flex-col gap-4">
             <Icons.Users className="w-12 h-12 text-slate-400" />
             <p className="text-4xl uppercase font-heading">club</p>
-            <p className="text-end text-primary-foreground/70">
+            <p className="text-end text-primary/70">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi,
               quisquam.
             </p>
@@ -134,7 +134,7 @@ export default function Page({}: Props) {
           <div className="bg-slate-900 relative rounded-3xl px-6 py-16 flex items-end flex-col gap-4">
             <Icons.Tv2 className="w-12 h-12 text-slate-400" />
             <p className="text-4xl uppercase font-heading">class</p>
-            <p className="text-end text-primary-foreground/70">
+            <p className="text-end text-primary/70">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi,
               quisquam.
             </p>
@@ -145,14 +145,14 @@ export default function Page({}: Props) {
         </div>
       </section>
 
-      <section className="container p-10 text-primary-foreground ">
+      <section className="container p-10 text-primary ">
         <h3 className="text-center font-heading text-6xl uppercase">
           Our Fitness class
         </h3>
-        <div className="flex gap-8 justify-center mt-10 font-heading text-3xl text-primary-foreground/50 uppercase">
+        <div className="flex gap-8 justify-center mt-10 font-heading text-3xl text-primary/50 uppercase">
           <span>all</span>
           <span>signature class</span>
-          <span className="text-primary-foreground decoration-orange-400 underline-offset-8 underline">
+          <span className="text-primary decoration-orange-400 underline-offset-8 underline">
             strenght and conditioning
           </span>
           <span>mind and body</span>
@@ -197,7 +197,7 @@ export default function Page({}: Props) {
       </section>
 
       <section className="container my-10 p-10 text-center flex justify-center relative">
-        <div className="flex flex-wrap font-heading text-4xl text-primary-foreground max-w-xl">
+        <div className="flex flex-wrap font-heading text-4xl text-primary max-w-xl">
           THE LAST THREE OR FOUR REPS IS WHAT MAKES THE MUSCLE GROW. THIS AREA
           OF PAIN DIVIDES A CHAMPION FROM SOMEONE WHO IS NOT A CHAMPION.
         </div>
@@ -211,20 +211,20 @@ export default function Page({}: Props) {
         />
       </section>
 
-      <section className="container font-heading p-10 bg-background rounded-[3rem] mt-4">
+      <section className="container font-heading p-10 text-primary-foreground bg-primary rounded-[3rem] mt-4">
         <h4 className="text-5xl uppercase text-center">
           Choose the right plan
         </h4>
-        <p className="text-center text-primary/80 mt-4">
+        <p className="text-center text-primary-foreground/80 mt-4">
           Choose the plan that suits you
         </p>
-        <div className="mt-6 text-primary-foreground bg-primary p-10 rounded-[3rem]">
+        <div className="mt-6 text-primary bg-primary-foreground p-10 rounded-[3rem]">
           <div className="py-6 border-b flex justify-between items-center">
             <div>
               <h4 className="text-5xl uppercase text-center">
                 starter fitness program
               </h4>
-              <p className="flex gap-4 ml-4 text-primary-foreground/80 mt-4">
+              <p className="flex gap-4 ml-4 text-primary/80 mt-4">
                 <span>5 Days a Week</span> <span>01 Sweat shirt</span>
                 <span>01 Bottle of protein</span>
               </p>
@@ -236,7 +236,7 @@ export default function Page({}: Props) {
               <h4 className="text-5xl uppercase text-center">
                 Beginner fitness program
               </h4>
-              <p className="flex gap-4 ml-4 text-primary-foreground/80 mt-4">
+              <p className="flex gap-4 ml-4 text-primary/80 mt-4">
                 <span>5 Days a Week</span> <span>01 Sweat shirt</span>
                 <span>01 Bottle of protein</span> <span>Access to videos</span>
               </p>
@@ -248,7 +248,7 @@ export default function Page({}: Props) {
               <h4 className="text-5xl uppercase text-center">
                 Advance fitness program
               </h4>
-              <p className="flex gap-4 ml-4 text-primary-foreground/80 mt-4">
+              <p className="flex gap-4 ml-4 text-primary/80 mt-4">
                 <span>5 Days a Week</span> <span>01 Sweat shirt</span>
                 <span>01 Bottle of protein</span> <span>Many more</span>
               </p>
@@ -260,7 +260,7 @@ export default function Page({}: Props) {
 
       <section className="container my-10 p-10 flex relative">
         <div className="flex justify-between items-center w-full">
-          <div className="flex flex-col font-heading text-6xl gap-2 text-primary-foreground max-w-xl uppercase">
+          <div className="flex flex-col font-heading text-6xl gap-2 text-primary max-w-xl uppercase">
             <p>GET a life style</p>
             <p>Easily and fun here</p>
           </div>
@@ -270,13 +270,12 @@ export default function Page({}: Props) {
         </div>
       </section>
 
-      <section className="container font-heading text-5xl text-primary-foreground/50 my-10 p-10 border-y-2 border-primary-foreground/50 uppercase">
-        feel great.{" "}
-        <span className="text-primary-foreground">body and mind.</span>
+      <section className="container font-heading text-5xl text-primary/50 my-10 p-10 border-y-2 border-primary-foreground/50 uppercase">
+        feel great. <span className="text-primary">body and mind.</span>
       </section>
 
-      <section className="flex justify-center gap-6 font-heading text-primary-foreground text-4xl">
-        <span className="text-primary-foreground/50">Email</span>
+      <section className="flex justify-center gap-6 font-heading text-primary text-4xl">
+        <span className="text-primary/50">Email</span>
         <span>Manishbishtdev@gmail.com</span>
       </section>
     </main>
